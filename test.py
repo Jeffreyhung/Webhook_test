03d47145f7bd8ca20d64b4cd7b3f8f35d51bbe0a
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2023-12-04 15:28:11 -0800

    test

--- test.py
+++ test.py
@@ -4,11 +4,18 @@ import json
 
 def main(input):
     # print("raw: "+input)
-    payload = json.loads(input)
+    # payload = json.loads(input)
     # print("json: "+str(payload))
     # print("actor: "+payload['actor'])
 
-    return "Hello "+payload['actor']+"!"
+    # return "Hello "+payload['actor']+"!"
+
+    message = "[getsentry/sentry](https://github.com/getsentry/sentry) "
+    message += "was moved to tag-non-production from tag-production by @jeffreyhung.\n\n "
+    message += "@jeffreyhung please review the changes for security-as-code terraform file."
+    message += "\n\n ❗ ❗ ❗  ** A `tf state rm` will be require before PR is merged**❗ ❗ ❗ "
+    return message
+
 
 if __name__ == '__main__':
     print(main(sys.argv[1]))
