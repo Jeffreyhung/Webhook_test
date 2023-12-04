45421b8aa2576a7ef6c40393e711d07de9148ed2
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2023-12-04 12:48:51 -0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import json
 def main(input):
     print("raw: "+input)
     payload = json.loads(input)
-    print("json: "+payload)
+    print("json: "+str(payload))
     print("actor: "+payload['actor'])
 
 if __name__ == '__main__':
