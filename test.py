24d303a1816eff67978eaab7e7b7d414ee0ab96b
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2023-12-05 14:28:36 -0800

    Update test.py

--- test.py
+++ test.py
@@ -10,11 +10,25 @@ def main(input):
 
     # return "Hello "+payload['actor']+"!"
     open("test.txt", "w").write("test")
+    payload = {
+        "action": "edited",
+        "actor": "jeffrey-sentry",
+        "new_tags": [
+        "debugtoolbar",
+        "tag-production"
+        ],
+        "repo": "getsentry/spotlight",
+        "tags": [
+        "debugtoolbar",
+        "tag-to-be-production-20231114"
+        ]
+    }
 
-    message = "[getsentry/sentry](https://github.com/getsentry/sentry) "
-    message += "was moved to tag-non-production from tag-production by @jeffreyhung.<br><br> "
-    message += "@jeffreyhung please review the changes for security-as-code terraform file."
-    message += "\<br><br> ❗ ❗ ❗  ** A `tf state rm` will be require before PR is merged**❗ ❗ ❗ "
+    message = f"[{payload['repo']}](https://github.com/{payload['repo']}) was moved to {payload['tags'][0]} from {payload['tags'][0]} by @{payload['actor']}."
+    message += f"\n\n {payload['actor']}please review the changes for security-as-code terraform file."
+
+    if True:
+        message += "\n\n ❗ ❗ ❗  **A `tf state rm` will be require before PR is merged**❗ ❗ ❗ "
     return message
 
 
